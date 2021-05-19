5822be1a239cc5f91b0394bb73b19a3dc46b6c16
Artur <Artur1Kuzmich@gmail.com>
2021-05-19 17:12:49 +0300

    create json server with data

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
         <Navbar />
-     <div className="invoice_content">
-         <Home />
-     </div>
+        <Home />
     </div>
   );
 }
