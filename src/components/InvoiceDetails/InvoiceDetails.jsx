002acc7ea5e4406a94afc7e57cf3c80d66b3eaad
Artur <Artur1Kuzmich@gmail.com>
2021-05-20 12:22:01 +0300

    create remove request

--- src/components/InvoiceDetails/InvoiceDetails.jsx
+++ src/components/InvoiceDetails/InvoiceDetails.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import './InvoiceDetailsStyle.scss';
 import useFetch from "../../utils/useFetch";
 
 const InvoiceDetails =() => {
     const { id } = useParams()
     const {data: invoices, load,} = useFetch('http://localhost:8000/invoices/' + id)
-    return(
-        <div>
+    const history = useHistory()
 
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/invoices/' + invoices._id, {
+            method: "DELETE",
+        } ).then(() => {
+            history.push('/')
+        })
+    }
+    return(
+        <div className="invoice_details">
+            {load && <div>...Loading</div>}
+            {invoices && (
+                <div className="details_inner">
+                    <div className="invoice_row">
+                        <div className='date'>Date Create: <span>{invoices.date_created}</span></div>
+                        <div className='date'>Date Supplied: <span>{invoices.date_supplied}</span></div>
+                    </div>
+                    <p className="invoice_comment">
+                        {invoices.comment}
+                    </p>
+                    <div className="invoice_row row_actions">
+                        <button className="action_btn">Edit</button>
+                        <button className="action_btn" onClick={handleDelete}>Delete</button>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
